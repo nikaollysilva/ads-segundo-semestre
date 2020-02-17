c35dffa235d0c128c1ffb60ab8f5d0ed5d1c3853
nikollysilvabandtec <nikollysilvabandtec>
2020-02-17 15:36:01 -0300

    Adicinando listas de exercicios 02

---
+++ Célia_LingProgram/src/LISTA02/Exer05_IMC.java
@@ -0,0 +1,59 @@
+package LISTA02;
+
+import java.util.Scanner;
+
+public class Exer05_IMC {
+    public static void main(String[] args) {
+
+        Double altura, peso, imc;
+        Integer sexo;
+
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Indice de Massa Corporal");
+        System.out.println("Insira sua altura:");
+        altura = leitor.nextDouble();
+        System.out.println("Insira se peso:");
+        peso = leitor.nextDouble();
+        System.out.println("Insira seu sexo: \n" +
+                "1- Feminino. \n" +
+                "2- Masculino.");
+        sexo = leitor.nextInt();
+
+        imc = peso /(altura * 2);
+
+        if (sexo == 1) {
+
+            if (imc < 19.1) {
+                System.out.println("Abaixo do peso");
+            } else if (imc > 19.1 && imc < 25.8) {
+                System.out.println("No peso normal");
+            } else if (imc > 25.8 && imc < 27.3) {
+                System.out.println("Marginalmente acima do peso");
+            } else if (imc > 27.3 && imc < 32.3) {
+                System.out.println("Acima do peso ideal");
+            } else {
+                System.out.println("Obeso");
+            }
+
+        } else if (sexo == 2) {
+
+            if (imc < 20.7) {
+                System.out.println("Abaixo do peso");
+            } else if (imc > 20.7 && imc < 26.4) {
+                System.out.println("No peso normal");
+            } else if (imc > 26.4 && imc < 27.8) {
+                System.out.println("Marginalmente acima do peso");
+            } else if (imc > 27.8 && imc < 31.1) {
+                System.out.println("Acima do peso ideal");
+            } else {
+                System.out.println("Obeso");
+            }
+
+        } else {
+            System.out.println("Selecione um dos Sexos Válidos");
+        }
+
+        System.out.println("Seu imc é: "+ imc);
+    }
+}
